4f912ca426d37b3e80aa6641d4a8279dba0d433c
rohanThegreaTT <rohanisdeadly@gmail.com>
2020-11-05 23:49:56 -0500

    cpp

--- bray/Bray_Engine.hpp
+++ bray/Bray_Engine.hpp
@@ -1,3 +1,3 @@
 #include "SDL2/SDL.h"
 #include "SDL2/SDL_image.h"
-#include "Bray_Window.h"
+#include "Bray_Window.hpp"
--- bray/main.cpp
+++ bray/main.cpp
@@ -1,4 +1,4 @@
-#include "Bray_Engine.h"
+#include "Bray_Engine.hpp"
 int main() {
     CreateWindow(500, 500, "Test");
     return 0;
